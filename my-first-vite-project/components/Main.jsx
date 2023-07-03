ff1a9b0d13ee4e1d4adf3d0cf1eae8f270771264
Eros-code <eros.achillea@outlook.com>
2023-07-03 13:43:36 +0100

    initial commit

---
+++ my-first-vite-project/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../src/App.css";
+
+export default function Main() {
+  return (
+    <div className="page-container">
+      <header>
+        <h1> Eros Achillea</h1>
+      </header>
+    </div>
+  );
+}
